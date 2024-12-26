7cebe348ea4fe2ed8107570df3c64c2eaefa4daf
Gowtham Ravichander <gowtham.ravi1011@gmail.com>
2024-12-26 10:48:52 +0000

    Edited the Page script to have two sections. Implemented a saved location list, and mapped that to output the weather cards for each of the saved locations.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,38 @@
 import WeatherCard from "@/components/weather_card/weather_card";
 
 export default function Home() {
+  const LocationList:string[] =[];
+  
+  
+
   return (
    <div>
-    <WeatherCard />
+      <div className="Current Location Content">
+        <div>
+          <h5>Current Location</h5>
+        </div>
+        <WeatherCard location="None"/>
+      </div>
+      <div className="Saved Location List">
+        {
+          LocationList.length != 0?
+            <div>
+              <h5>Saved Locations</h5>
+            </div>:
+            <div>
+            </div>
+        }
+
+        <div>
+          {LocationList.map((locationValue:string) => {
+            return(
+              <WeatherCard location={locationValue} />
+            )
+            })
+          }
+        </div>
+      </div>
+    
    </div>
   );
 }
