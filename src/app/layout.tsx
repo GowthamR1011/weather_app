596311e81e6b433eed48adf20076176a3071bd41
Gowtham Ravichander <gowtham.ravi1011@gmail.com>
2024-12-30 00:32:45 +0000

    Added material design by tailwindcss for the project. This is used by the toggle switches.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+     
+      {/* <script async src="node_modules/@material-tailwind/html/scripts/ripple.js"></script>
+      
+
+      <script async src="https://unpkg.com/@material-tailwind/html@latest/scripts/ripple.js"></script> */}
+
       <body>
         <Header />
         {children}
