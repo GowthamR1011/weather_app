1ba886f5d6fdd335928c55d5380680d8f0ffb9d1
Gowtham Ravichander <gowtham.ravi1011@gmail.com>
2025-02-21 14:52:28 +0000

    Setup geolocation API to get the current location of the device

--- src/components/Buttons/LocationButton.tsx
+++ src/components/Buttons/LocationButton.tsx
@@ -1,12 +1,29 @@
+"use client"
 import * as React from 'react';
 import { MdMyLocation } from "react-icons/md";
 interface ILocationButtonProps {
 }
-
+interface ILocation{
+  latitude:number;
+  longitude:number;
+}
 const LocationButton: React.FunctionComponent<ILocationButtonProps> = (props) => {
+  
+  const [location,setLocation] = React.useState<ILocation>();
+  function getLocation(){
+    if('geolocation' in navigator)
+      navigator.geolocation.getCurrentPosition(({ coords }) => {
+        const { latitude, longitude } = coords;
+        setLocation({ latitude, longitude });
+        console.log(latitude);
+    })
+  }
+
+  React.useEffect(getLocation,[])
+  
   return(
     <div className="mx-2 cursor-pointer">
-        <MdMyLocation />
+        <MdMyLocation onClick={getLocation}/>
     </div>
   );
 };
